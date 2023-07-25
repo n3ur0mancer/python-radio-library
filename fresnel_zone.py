a2362a532af4df291e296e447fa035fef8b4afaf
n3ur0mancer <46748400+n3ur0mancer@users.noreply.github.com>
2023-07-25 16:34:22 +0200

    Added simple fresnel zone calculator

---
+++ fresnel_zone.py
@@ -0,0 +1,19 @@
+import math
+
+
+class FresnelZone:
+    def __init__(self, wavelength_meter, distance_meter):
+        self.wavelength_meter = wavelength_meter
+        self.distance_meter = distance_meter
+
+    def calc_fresnel_zone(self):
+        return ((math.sqrt(self.wavelength_meter * self.distance_meter)) / 2) * 0.6
+
+
+input_wavelength_meter = float(input("Wavelength in meter: "))
+input_distance_meter = float(input("Distance between stations in meter: "))
+
+new_fresnel_zone = FresnelZone(input_wavelength_meter, input_distance_meter)
+fresnel_result = new_fresnel_zone.calc_fresnel_zone()
+
+print(f"The Fresnel zone is: {fresnel_result} m")
