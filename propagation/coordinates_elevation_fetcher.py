4e38cac2fee344ca681cd6b2fc33bf16cf28fc36
n3ur0mancer <46748400+n3ur0mancer@users.noreply.github.com>
2023-07-28 20:06:20 +0200

    Created class to fetch data

--- propagation/coordinates_elevation_fetcher.py
+++ propagation/coordinates_elevation_fetcher.py
@@ -1,35 +1,40 @@
 import coordinate_interpolation
 import requests
 
-latitude_a = 47.684282
-longitude_a = 9.640046
-latitude_b = 47.652013
-longitude_b = 9.525812
-granularity_meters = 250
 
-new_instance = coordinate_interpolation.CoordinateInterpolation(latitude_a, longitude_a, latitude_b, longitude_b, granularity_meters)
-locations_list = new_instance.interpolate_coordinates()
+class CoordinateElevationFetcher:
+    def __init__(self, latitude_point_a, longitude_point_a, latitude_point_b, longitude_point_b, granularity_meters):
+        self.latitude_point_a = latitude_point_a
+        self.longitude_point_a = longitude_point_a
+        self.latitude_point_b = latitude_point_b
+        self.longitude_point_b = longitude_point_b
+        self.granularity_meters = granularity_meters
 
+    def interpolate_coordinates(self):
+        new_instance = coordinate_interpolation.CoordinateInterpolation(self.latitude_point_a, self.longitude_point_a,
+                                                                        self.latitude_point_b, self.longitude_point_b,
+                                                                        self.granularity_meters)
+        return new_instance.interpolate_coordinates()
 
-def get_elevation(locations):
-    api_url = "https://api.open-elevation.com/api/v1/lookup"
+    def get_elevation(self, locations):
+        api_url = "https://api.open-elevation.com/api/v1/lookup"
 
-    try:
-        response = requests.post(api_url, json={"locations": locations})
-        response.raise_for_status()
-        data = response.json()
+        try:
+            response = requests.post(api_url, json={"locations": locations})
+            response.raise_for_status()
+            data = response.json()
 
-        elevations = [result["elevation"] for result in data["results"]]
-        return elevations
-    except requests.exceptions.RequestException as e:
-        print(f"Error occurred: {e}")
-        return None
+            elevations = [result["elevation"] for result in data["results"]]
+            return elevations
+        except requests.exceptions.RequestException as e:
+            print(f"Error occurred: {e}")
+            return None
 
+    def get_elevation_data(self):
+        locations_list = self.interpolate_coordinates()
+        elevations = self.get_elevation(locations_list)
 
-elevations = get_elevation(locations_list)
+        elevation_data = [{"latitude": loc["latitude"], "longitude": loc["longitude"], "elevation": elev}
+                          for loc, elev in zip(locations_list, elevations)]
 
-for location in range(len(locations_list)):
-    locations_list[location]["elevation"] = elevations[location]
-
-for location in locations_list:
-    print(location)
+        return elevation_data
