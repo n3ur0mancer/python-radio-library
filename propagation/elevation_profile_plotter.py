7391e0efc0426d2cb41c8e179f74371bf673f075
n3ur0mancer <46748400+n3ur0mancer@users.noreply.github.com>
2023-07-28 22:36:35 +0200

    Adapted plot styling

--- propagation/elevation_profile_plotter.py
+++ propagation/elevation_profile_plotter.py
@@ -17,16 +17,31 @@ class ElevationProfilePlotter:
         latitudes = [item['latitude'] for item in elevation_data]
         elevations = [item['elevation'] for item in elevation_data]
 
-        # Create the area chart
-        plt.fill_between(latitudes, elevations, color='skyblue')
-        plt.plot(latitudes, elevations, color='blue', label='Elevation')
-        plt.title('Elevation along Latitude')
-        plt.xlabel('Latitude')
-        plt.ylabel('Elevation (meters)')
-        plt.legend()
-        plt.grid(False)
+        # plt.fill_between(latitudes, elevations, color='#FFA500', alpha=0.75)
+        plt.figure(facecolor="#000000")
+        ax = plt.axes()
+        ax.set_facecolor("#000000")
+        plt.grid(True, linestyle='dashed', linewidth=0.5, color='gray')
+
+        spines = plt.gca().spines
+        for spine in spines.values():
+            spine.set_visible(True)
+            spine.set_color('white')
+            spine.set_linestyle('-')
+
+        plt.plot(latitudes, elevations, color='#FFA500', linewidth=0.5, clip_on=True, label='Elevation')
+
+        plt.title('Elevation along Latitude', color='white', loc='left', font="Roboto", fontsize=15)
+        plt.xlabel('Latitude', color='white', font="Roboto")
+        plt.xticks(color='white', font="Roboto")
+        plt.ylabel('Elevation (meters)', color='white', font="Roboto")
+        plt.yticks(color='white', font="Roboto")
+
+        plt.legend(facecolor="black", labelcolor="white", prop={'family': 'Roboto'})
+        plt.xlim(min(latitudes), max(latitudes))
+
         plt.show()
 
 
-new_plotter = ElevationProfilePlotter(47.502136, 9.235879, 47.711853, 9.647965, 100)
+new_plotter = ElevationProfilePlotter(47.502136, 9.235879, 47.711853, 9.647965, 200)
 new_plotter.plot_elevation_profile()
