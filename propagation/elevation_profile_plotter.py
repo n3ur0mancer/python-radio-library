3fa03a30cea530e0fdcb5876b7630ac74da71183
n3ur0mancer <46748400+n3ur0mancer@users.noreply.github.com>
2023-07-28 21:42:30 +0200

    Created class for ElevationProfilePlotter

--- propagation/elevation_profile_plotter.py
+++ propagation/elevation_profile_plotter.py
@@ -1,24 +1,32 @@
 import matplotlib.pyplot as plt
 import coordinates_elevation_fetcher
 
-latitude_a = 49.562072
-longitude_a = 5.863934
-latitude_b = 49.543993
-longitude_b = 5.830670
-granularity_meters = 50
 
-data_fetcher = coordinates_elevation_fetcher.CoordinateElevationFetcher(latitude_a, longitude_a, latitude_b, longitude_b, granularity_meters)
-elevation_data = data_fetcher.get_elevation_data()
+class ElevationProfilePlotter:
+    def __init__(self, latitude_point_a, longitude_point_a, latitude_point_b, longitude_point_b, granularity_meters):
+        self.latitude_point_a = latitude_point_a
+        self.longitude_point_a = longitude_point_a
+        self.latitude_point_b = latitude_point_b
+        self.longitude_point_b = longitude_point_b
+        self.granularity_meters = granularity_meters
 
-latitudes = [item['latitude'] for item in elevation_data]
-elevations = [item['elevation'] for item in elevation_data]
+    def plot_elevation_profile(self):
+        data_fetcher = coordinates_elevation_fetcher.CoordinateElevationFetcher(self.latitude_point_a, self.longitude_point_a, self.latitude_point_b, self.longitude_point_b, self.granularity_meters)
+        elevation_data = data_fetcher.get_elevation_data()
 
-# Create the area chart
-plt.fill_between(latitudes, elevations, color='skyblue')
-plt.plot(latitudes, elevations, color='blue', label='Elevation')
-plt.title('Elevation along Latitude')
-plt.xlabel('Latitude')
-plt.ylabel('Elevation (meters)')
-plt.legend()
-plt.grid(False)
-plt.show()
+        latitudes = [item['latitude'] for item in elevation_data]
+        elevations = [item['elevation'] for item in elevation_data]
+
+        # Create the area chart
+        plt.fill_between(latitudes, elevations, color='skyblue')
+        plt.plot(latitudes, elevations, color='blue', label='Elevation')
+        plt.title('Elevation along Latitude')
+        plt.xlabel('Latitude')
+        plt.ylabel('Elevation (meters)')
+        plt.legend()
+        plt.grid(False)
+        plt.show()
+
+
+new_plotter = ElevationProfilePlotter(47.502136, 9.235879, 47.711853, 9.647965, 100)
+new_plotter.plot_elevation_profile()
